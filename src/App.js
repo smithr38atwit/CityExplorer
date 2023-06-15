0b4bad8c98866fa9d05e83118441cb354b897ab7
Ryan Smith <smithr38@wit.edu>
2023-06-15 17:06:04 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import mapboxgl from 'mapbox-gl';
+import { useEffect } from 'react';
+
+mapboxgl.accessToken = "pk.eyJ1Ijoic2V2ZXJvbWFyY3VzIiwiYSI6ImNsaHRoOWN0bzAxOXIzZGwxaGl3M2NydGcifQ.xl99wY4570Gg6hh7F7tOxA";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    useEffect(() => {
+        var map = new mapboxgl.Map({
+            container: "map",
+            style: "mapbox://styles/mapbox/streets-v11",
+            center: [-71.095019, 42.336611],
+            zoom: 14,
+        });
+
+        return () => {
+            map.remove();
+        };
+    }, []);
+
+    return (
+        <div className="App">
+            <div id='map' style={{ width: 400, height: 300 }}></div>
+        </div>
+    );
 }
 
 export default App;
