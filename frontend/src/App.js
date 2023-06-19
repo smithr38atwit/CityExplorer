7beb32aef9ac97498a16ee27767cddf66fc79923
Peter Paravalos <90714330+Paravalosp@users.noreply.github.com>
2023-06-19 18:24:47 -0400

    like making it do

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,10 +8,11 @@ mapboxgl.accessToken = "pk.eyJ1Ijoic2V2ZXJvbWFyY3VzIiwiYSI6ImNsaHRoOWN0bzAxOXIzZ
 
 function App() {
     const [menuOpen, setMenuOpen] = useState(false);
-
+   
     const toggleMenu = () => {
         setMenuOpen(!menuOpen); // Toggle the value of menuOpen
     };
+
     useEffect(() => {
         var map = new mapboxgl.Map({
             container: "map",
@@ -33,6 +34,13 @@ function App() {
             mapboxgl: mapboxgl,
             placeholder: "Search for a location",
         }));
+        var marker = new mapboxgl.Marker({ 
+            color: "#FF0000", 
+            draggable: false 
+          })
+            .setLngLat([-71.0596, 42.3601])
+            .addTo(map);
+
 
         return () => {
             map.remove();
@@ -43,14 +51,14 @@ function App() {
         <div className="App">
             <div style={{ position: 'relative', width: '100%', height: '700px' }}>
                 <div id='map' style={{ width: '100%', height: '100%' }}></div>
-                <div style={{ position: 'absolute', top: '10px', left: '10px', zIndex: '1' }}>
+                <div style={{ position: 'absolute', bottom: '35px', left: '10px', zIndex: '1' }}>
                     <button onClick={toggleMenu}>Open Menu</button>
                     {menuOpen && (
-                        <ul>
-                            <li>NUMERO UNO</li>
-                            <li>RANK</li>
-                            <li>QUEST3</li>
-                        </ul>
+                          <ul style={{ position: 'absolute', top: '-130px', background: '#fff', padding: '10px', boxShadow: '0 0 5px rgba(0, 0, 0, 0.3)' }}>
+                          <button>Account</button>
+                          <button>Quests</button>
+                          <button>Friends</button>
+                      </ul>
                     )}
                 </div>
             </div>
