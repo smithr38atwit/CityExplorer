0dc6280584b1edd4c5991601555fc207e7bd70ff
Peter Paravalos <90714330+Paravalosp@users.noreply.github.com>
2023-06-20 15:17:21 -0400

    + local test data, +AQF tabs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,19 +6,50 @@ import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 
 mapboxgl.accessToken = "pk.eyJ1Ijoic2V2ZXJvbWFyY3VzIiwiYSI6ImNsaHRoOWN0bzAxOXIzZGwxaGl3M2NydGcifQ.xl99wY4570Gg6hh7F7tOxA";
 
+const userData = {
+    name: "John Doe",
+    email: "johndoe@example.com",
+    location: "New York",
+    avatar: "https://example.com/avatar.jpg"
+  };
+const questData = [
+    { id: 1, name: "Collect 10 gems", status: "In Progress" },
+    { id: 2, name: "Defeat the dragon", status: "Completed" },
+    { id: 3, name: "Explore the hidden cave", status: "Not Started" }
+  ];
+
+const friendData = [
+    { id: 1, name: "Alice" },
+    { id: 2, name: "Bob" },
+    { id: 3, name: "Charlie" }
+  ];
+
 
 function App() {
     const [menuOpen, setMenuOpen] = useState(false);
-    const [loggedIn, setLoggedIn] = useState(false);
+    const [userDataVisible, setUserDataVisible] = useState(false);
+    const [displayQuests, setDisplayQuests] = useState(false);
+    const [displayFriends, setDisplayFriends] = useState(false);
 
     const toggleMenu = () => {
-        setMenuOpen(!menuOpen); // Toggle the value of menuOpen
+        setMenuOpen(!menuOpen);
+        setUserDataVisible(false); // Toggle the value of menuOpen
+        setDisplayQuests(false);
+        setDisplayFriends(false);
     };
-    const handleLogin = () => {
-        setLoggedIn(true);
-    };
+    const toggleUserData = () => {
+        setUserDataVisible(!userDataVisible); // Toggle the value of userDataVisible
+      };
+    const toggleDisplayQuests = () => {
+        setDisplayQuests(!displayQuests); // Toggle the value of displayQuests
+      };
+
+    const toggleDisplayFriends = () => {
+        setDisplayFriends(!displayFriends); // Toggle the value of displayFriends
+      };
+
+
     useEffect(() => {
-        if(loggedIn){}
         var map = new mapboxgl.Map({
             container: "map",
             style: "mapbox://styles/mapbox/streets-v11",
@@ -41,45 +72,36 @@ function App() {
                 mapboxgl: mapboxgl,
                 placeholder: "Search for a location",
             });
+
             map.addControl(geocoder, 'top-left');
 
-            map.setFog({
-                color: 'rgb(186, 210, 235)', // Lower atmosphere
-                'high-color': 'rgb(36, 92, 223)', // Upper atmosphere
-                'horizon-blend': 0.02, // Atmosphere thickness (default 0.2 at low zooms)
-                'space-color': 'rgb(11, 11, 25)', // Background color
-                'star-intensity': 0.6 // Background star brightness (default 0.35 at low zoooms )
-            });
+            // map.setFog({
+            //     color: 'rgb(186, 210, 235)', // Lower atmosphere
+            //     'high-color': 'rgb(36, 92, 223)', // Upper atmosphere
+            //     'horizon-blend': 0.02, // Atmosphere thickness (default 0.2 at low zooms)
+            //     'space-color': 'rgb(11, 11, 25)', // Background color
+            //     'star-intensity': 0.6 // Background star brightness (default 0.35 at low zoooms )
+            // });
 
-            map.addLayer({
-                id: 'building-heights',
-                type: 'fill-extrusion',
-                source: 'composite',
-                'source-layer': 'building',
-                paint: {
-                    'fill-extrusion-color': '#888888',
-                    'fill-extrusion-height': ['get', 'height'],
-                    'fill-extrusion-base': 0,
-                    'fill-extrusion-opacity': 0.6
-                }
-            });
+            // map.addLayer({
+            //     id: 'building-heights',
+            //     type: 'fill-extrusion',
+            //     source: 'composite',
+            //     'source-layer': 'building',
+            //     paint: {
+            //         'fill-extrusion-color': '#888888',
+            //         'fill-extrusion-height': ['get', 'height'],
+            //         'fill-extrusion-base': 0,
+            //         'fill-extrusion-opacity': 0.6
+            //     }
+            // });
             const popup = new mapboxgl.Popup({
                 closeButton: false
             });
             geocoder.on('result', (e) => {
                 const { result } = e;
-
                 popup.setLngLat(result.center)
                     .setHTML(`<h3>${result.place_name}</h3>`)
-                    .addTo(map);
-            });
-            map.on('mousedown', (e) => {
-                const coordinates = e.lngLat;
-                const marker = new mapboxgl.Marker({
-                    color: "#0000FF",
-                    draggable: false
-                })
-                    .setLngLat(coordinates)
                     .addTo(map);
             });
             var marker = new mapboxgl.Marker({
@@ -98,20 +120,48 @@ function App() {
 
     return (
         <div className="App">
-            <div style={{ position: 'relative', width: '100%', height: '700px' }}>
-                <div id='map' style={{ width: '100%', height: '100%' }}></div>
-                <div style={{ position: 'absolute', bottom: '35px', left: '10px', zIndex: '1' }}>
-                    <button onClick={toggleMenu}>Open Menu</button>
-                    {menuOpen && (
-                          <ul style={{ position: 'absolute', top: '-25px', left: '100px', background: '#fff', padding: '10px', boxShadow: '0 0 5px rgba(0, 0, 0, 0.3)', display: 'flex', listStyle: 'none'}}>
-                          <button>Account</button>
-                          <button>Quests</button>
-                          <button>Friends</button>
-                      </ul>
-                    )}
+      <div style={{ position: 'relative', width: '100%', height: '100vh' }}>
+        <div id='map' style={{ width: '100%', height: '100%' }}></div>
+        <div style={{ position: 'absolute', bottom: '35px', left: '10px', zIndex: '1' }}>
+          <button onClick={toggleMenu}>Open Menu</button>
+            {menuOpen && (
+              <ul style={{ position: 'absolute', top: '-25px', left: '100px', background: '#fff', padding: '10px', boxShadow: '0 0 5px rgba(0, 0, 0, 0.3)', display: 'flex', listStyle: 'none'}}>
+                <button onClick={toggleUserData}>Account</button>
+                <button onClick={toggleDisplayQuests}>Quests</button>
+                <button onClick={toggleDisplayFriends}>Friends</button>
+              </ul>
+            )}
+            {userDataVisible && (
+              <div style={{ position: 'absolute', top: '-250px', right: '-150px', backgroundColor: '#fff', padding: '20px', border: '1px solid #ccc' }}>
+                <h2>User Data</h2>
+                <p>Name: {userData.name}</p>
+                <p>Email: {userData.email}</p>
+                <p>Location: {userData.location}</p>
+              </div>
+            )}
+            {displayQuests && (
+              <div style={{ position: 'absolute', top: '-250px', right: '-475px', backgroundColor: '#fff', padding: '20px', border: '1px solid #ccc' }}>
+                <h2>Quests</h2>
+                <ul>
+                {questData.map((quest) => (
+                    <li key={quest.id}>{quest.name} - {quest.status}</li>
+                  ))}
+                </ul>
                 </div>
-            </div>
+              )}
+            {displayFriends && (
+              <div style={{ position: 'absolute', top: '-250px', right: '-620px', backgroundColor: '#fff', padding: '20px', border: '1px solid #ccc' }}>
+                  <h2>Friends</h2>
+                  <ul>
+                    {friendData.map(friend => (
+                      <li key={friend.id}>{friend.name}</li>
+                    ))}
+                  </ul>
+                </div>
+              )}
         </div>
+      </div>
+    </div>
     );
 }
 
