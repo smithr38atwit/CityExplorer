fa26e20df49a83efcf0dbfa308d3cbee1568c8be
Ryan Smith <smithr38@wit.edu>
2023-06-26 22:49:09 -0400

    added to sign up form

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,40 +1,49 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faWarning } from '@fortawesome/free-solid-svg-icons';
+
 import { createAccount } from '../scripts/api';
 import "./Login.css";
 
-const LoginPopup = () => {
-    const [formData, setFormData] = useState({
-        username: '',
-        email: '',
-        password: '',
-        confirmPassword: '',
-        rememberMe: false
-    });
+function LoginPopup() {
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [rememberMe, setRememberMe] = useState(false);
+
+    const [validConfirm, setValidConfirm] = useState(false);
+    const [confirmFocus, setConfirmFocus] = useState(false);
+
     const [showCreateAccount, setShowCreateAccount] = useState(false);
+    const [displayPopup, setDisplayPopup] = useState(true);
 
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData((prevData) => ({
-            ...prevData,
-            [name]: value,
-        }));
-    };
+    useEffect(() => {
+        const match = password === confirmPassword;
+        setValidConfirm(match);
+    }, [password, confirmPassword]);
+
 
     const handleLogin = (e) => {
         e.preventDefault();
         // Perform login logic using loginEmail and loginPassword
     };
 
-    const handleCreateAccount = (e) => {
+    const handleCreateAccount = async (e) => {
         e.preventDefault();
-        const response = createAccount(formData.username, formData.email, formData.password);
-        if (response.ok) {
-            console.debug("Account created successfully")
-        } else if (response.status === 400) {
-            console.debug("Email already in use")
-        } else {
-            console.debug("Account could not be created")
+        try {
+            const response = await createAccount(username, email, password);
+            if (response.ok) {
+                console.debug("Account created successfully")
+                setDisplayPopup(false);
+            } else if (response.status === 400) {
+                console.debug("Email already in use")
+            } else {
+                console.debug("Account could not be created")
+            }
+        } catch (error) {
+
         }
     };
 
@@ -42,24 +51,19 @@ const LoginPopup = () => {
         setShowCreateAccount(!showCreateAccount);
     };
 
-    const closePopup = () => {
-        document.getElementById("login-popup-container").style.display = 'none';
-    };
-
-
     return (
-        <div id='login-popup-container' className="popup-container">
+        <div className="popup-container" style={{ display: displayPopup ? "block" : "none" }}>
             {showCreateAccount ? (
                 <div id='create-account' className="popup">
-                    <p className='close-popup' onClick={closePopup}>X</p>
+                    <p className='close-popup' onClick={() => setDisplayPopup(false)}>X</p>
                     <h2>Create Account</h2>
                     <form onSubmit={handleCreateAccount}>
                         <input
                             type="text"
                             name='username'
                             placeholder="Username"
-                            value={formData.username}
-                            onChange={handleChange}
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
                             required
                             autoComplete='username'
                         />
@@ -67,8 +71,8 @@ const LoginPopup = () => {
                             type="email"
                             name='email'
                             placeholder="Email"
-                            value={formData.email}
-                            onChange={handleChange}
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
                             required
                             autoComplete='email'
                         />
@@ -76,8 +80,8 @@ const LoginPopup = () => {
                             type="password"
                             name='password'
                             placeholder="Password"
-                            value={formData.password}
-                            onChange={handleChange}
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
                             required
                             autoComplete='new-password'
                         />
@@ -85,34 +89,39 @@ const LoginPopup = () => {
                             type="password"
                             name='confirmPassword'
                             placeholder="Confirm Password"
-                            value={formData.confirmPassword}
-                            onChange={handleChange}
+                            value={confirmPassword}
+                            onChange={(e) => setConfirmPassword(e.target.value)}
                             required
                             autoComplete='new-password'
+                            onFocus={() => setConfirmFocus(true)}
+                            onBlur={() => setConfirmFocus(false)}
                         />
+                        <p className='confirmnote' style={{ display: confirmFocus && !validConfirm ? 'flex' : 'none' }}>
+                            <FontAwesomeIcon icon={faWarning} /><span>Passwords must match</span>
+                        </p>
                         <label>
                             <input
                                 type="checkbox"
-                                checked={formData.rememberMe}
-                                onChange={() => setFormData(prevData => ({ ...prevData, rememberMe: !formData.rememberMe }))}
+                                checked={rememberMe}
+                                onChange={() => setRememberMe(!rememberMe)}
                             />
                             Remember me
                         </label>
-                        <button type="submit">Create Account</button>
+                        <button type="submit" disabled={!validConfirm}>Create Account</button>
                         <p>Already have an account? <button onClick={togglePopup}>LOG IN</button></p>
                     </form>
                 </div>
             ) : (
                 <div id='login' className="popup">
-                    <p className='close-popup' onClick={closePopup}>X</p>
+                    <p className='close-popup' onClick={() => setDisplayPopup(false)}>X</p>
                     <h2>Login</h2>
                     <form onSubmit={handleLogin}>
                         <input
                             type="email"
                             name='email'
                             placeholder="Email"
-                            value={formData.email}
-                            onChange={handleChange}
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
                             required
                             autoComplete='email'
                         />
@@ -120,20 +129,20 @@ const LoginPopup = () => {
                             type="password"
                             name='password'
                             placeholder="Password"
-                            value={formData.password}
-                            onChange={handleChange}
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
                             required
                             autoComplete='current-password'
                         />
                         <label>
                             <input
                                 type="checkbox"
-                                checked={formData.rememberMe}
-                                onChange={() => setFormData(prevData => ({ ...prevData, rememberMe: !formData.rememberMe }))}
+                                checked={rememberMe}
+                                onChange={() => setRememberMe(!rememberMe)}
                             />
                             Remember me
                         </label>
-                        <button type="submit">Login</button>
+                        <button type="submit" disabled={!validConfirm}>Login</button>
                         <p>No account? <button onClick={togglePopup}>CREATE ONE</button></p>
                     </form>
                 </div>
--- frontend/src/scripts/api.js
+++ frontend/src/scripts/api.js
@@ -1,7 +1,7 @@
 const api = "http://127.0.0.1:8000"
 
 
-export async function createAccount(username, email, password) {
+export function createAccount(username, email, password) {
     const url = `${api}/users/`;
     const body = JSON.stringify({
         "username": username,
@@ -9,16 +9,11 @@ export async function createAccount(username, email, password) {
         "password": password
     })
     console.debug(`POST: ${url}\n`, body);
-    let response;
-    try {
-        response = await fetch(url, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: body
-        });
-    } catch (error) {
-        console.debug("FETCH ERROR: ", error)
-    }
+    const response = fetch(url, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: body
+    });
     return response;
 }
 
