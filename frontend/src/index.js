7413ca2c66deca40d3aac8ad5eaedb397f61b3f7
Ryan Smith <smithr38@wit.edu>
2023-07-11 17:02:54 -0400

    changed map from ref to context

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { AuthProvider } from './context/AuthProvider';
+import { MapProvider } from './context/MapProvider';
 import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <MapProvider>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </MapProvider>
   </React.StrictMode>
 );
