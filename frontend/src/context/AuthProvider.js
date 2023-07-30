c8417c079c2852dac31c3cc0a590ee71a9010f60
Peter Paravalos <90714330+Paravalosp@users.noreply.github.com>
2023-07-30 18:11:36 -0400

    a lot of fucking shit

--- frontend/src/context/AuthProvider.js
+++ frontend/src/context/AuthProvider.js
@@ -4,7 +4,6 @@ const AuthContext = createContext({});
 
 export function AuthProvider({ children }) {
     const [auth, setAuth] = useState({});
-
     return (
         <AuthContext.Provider value={{ auth, setAuth }}>
             {children}
